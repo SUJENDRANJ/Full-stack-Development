682c0024060d226de057d3b3a575ef4259f5c7d4
Suje <sujendranjanagiraman@gmail.com>
2024-08-02 17:32:02 +0530

    day 5 angular

--- ANGULAR/Employee-Management-System/src/app/app.component.ts
+++ ANGULAR/Employee-Management-System/src/app/app.component.ts
@@ -12,10 +12,14 @@ export class AppComponent {
 
   employee: Employee;
   result: string;
+  employeeArr: Employee[];
+  flag: boolean;
 
   constructor(private service: EmployeeService) {
     this.employee = new Employee();
     this.result = '';
+    this.employeeArr = [];
+    this.flag = false;
   }
 
   insertEmployee(data: any) {
@@ -26,5 +30,31 @@ export class AppComponent {
     alert(data.empId + ' ' + data.empName + ' ' + data.empSalary);
 
     this.result = this.service.insertEmployee(this.employee);
+  }
+
+  updateEmployee(data: any) {
+    this.employee.id = data.empId;
+    this.employee.empName = data.empName;
+    this.employee.empSalary = data.empSalary;
+
+    alert(data.empId + ' ' + data.empName + ' ' + data.empSalary);
+
+    this.result = this.service.updateEmployee(this.employee);
+  }
+  deleteEmployee(data: any) {
+    this.result = this.service.deleteEmployee(data.empId);
+  }
+  findEmployee(data: any) {
+    this.employee = this.service.findEmployee(data.empId);
+    this.result =
+      this.employee.id +
+      ' ' +
+      this.employee.empName +
+      ' ' +
+      this.employee.empSalary;
+  }
+  findAllEmployee() {
+    this.employeeArr = this.service.findAllEmployee();
+    this.flag = true;
   }
 }
